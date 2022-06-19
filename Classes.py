4e06d17c350032951c5dc2e48f2b8c944dd94ee0
PajudpodAlbertoJr <61365883+PajudpodAlbertoJr@users.noreply.github.com>
2022-06-19 11:03:51 +0800

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -4,10 +4,16 @@ p1 = Var
 print(p1.x)
 
 class Tao:
-    def __int__(self, name, age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def Greet(self):
+        print("Hello my name is {}, aged {} years old".format(self.name,self.age))
+
+
 p2 = Tao("Hello", 17)
 print(p2)
 print(p2.age)
 print(p2.name)
+p2.Greet()
