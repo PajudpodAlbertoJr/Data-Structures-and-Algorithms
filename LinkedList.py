0b57e4db60960781b81c3c0baf797bdc1fd839e1
PajudpodAlbertoJr <61365883+PajudpodAlbertoJr@users.noreply.github.com>
2022-06-26 22:53:34 +0800

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -14,10 +14,38 @@ class LList:
     def printl(self):
         temp = self.head
         while (temp):
-            print(temp.data,end=" ")
+            print(temp.data,end="  ")
+            print()
             temp = temp.next
+    def pop(self,x):
+        temp = self.head
+
+        if (temp is not None):
+            if (temp.data==x):
+                self.head = temp.next
+                temp = None
+                return 
+        while(temp is not None):
+            if temp.data==x:
+                break
+            prev = temp
+            temp = temp.next
+
+        if (temp == None):
+            return
+        prev.next = temp.next
+
+        temp = None
 #if __name__== '__main__':
 llist= LList()
 llist.push(3)    
 llist.push(3)
+llist.push(4)
+llist.push(5)
+llist.push(6)
+llist.push(7)
+llist.push(8)
+
+llist.printl()
+llist.pop(5)
 llist.printl()
